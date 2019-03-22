5cc9b73fd893b3b8c1dcd90825fb01a574a51c2f
Ndoye, Babacar <bn394315@ohio.edu>
2019-03-22 12:34:22 -0400

    changes

--- main.cc
+++ main.cc
@@ -11,9 +11,15 @@ int main()
 
     char ans;
     srand (time(NULL));
-    double div= rand() %2+1;
+    double div = 1;
+    while(div == 1){
+        //Dividing by 1 breaks the code
+        cout << "hi" << endl;
+        div = rand() %2+1;
+    }
+    
     double num = rand() % 20+4;
-
+    
     cout<<setw(5)<<"****YOU HAVE TO USE A PENCIL AND PAPER FOR THIS EXERCISE OR YOUR MIND****"<<endl;
     cout << "Step 1. Think of number: " << endl;
     cout<<"Step 2. Multiply is by two: "<<endl;
@@ -26,20 +32,17 @@ int main()
 
     while(i)
     {
-        if(ans=='Y' || ans=='y')
-    {
-
-            cout<<"The secret number is: "<< num/2<<endl;
-
-            cout<<"Thank you for playing."<<endl;
-            i=0;
-
-    }
+        if(ans == 'Y' || ans == 'y')
+        {
+            cout << "The secret number is: " << num/2 <<endl;
+            cout << "Thank you for playing." << endl;
+            i = 0;
+        }  
         else
-            {
-                cout<<"Thank you for playing."<<endl;
-                i=0;
-            }
+        {
+            cout << "Thank you for playing." <<endl;
+            i = 0;
+        }
     }
 
 }
