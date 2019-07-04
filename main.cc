8ca64d053813b672650542aa8e706c1ce9374b99
Babacar Ndoye <babacarndoye13@gmail.com>
2019-07-04 03:25:45 -0400

    main

--- main.cc
+++ main.cc
@@ -7,39 +7,39 @@ using namespace std;
 int main()
 {
     char ans;
-    srand (time(NULL));
+    srand(time(NULL));
     double div = 1;
-    while(div == 1){
+    while (div == 1)
+    {
         //Dividing by 1 breaks the code
         cout << "hi" << endl;
-        div = rand() %2+1;
+        div = rand() % 2 + 1;
     }
-    double num = rand() % 20+4;
-    
-    cout<<setw(5)<<"****YOU HAVE TO USE A PENCIL AND PAPER FOR THIS EXERCISE OR YOUR MIND****"<<endl;
-    cout << "Step 1. Think of number: " << endl;
-    cout<<"Step 2. Multiply is by two: "<<endl;
-    cout<<"Step 3. Add: "<<num<<endl;
-    cout<<"Step 4. Divide it by: "<<div<<endl;
-    cout<<"Step 5. Subtract your original starting number: "<<endl;
+    double num = rand() % 20 + 4;
 
-    cout<<"when you are ready for me to tell you my number: press(Y)"<<endl;
-    cin>>ans;
+    cout << setw(5) << "****YOU HAVE TO USE A PENCIL AND PAPER FOR THIS EXERCISE OR YOUR MIND****" << endl;
+    cout << "Step 1. Think of number: " << endl;
+    cout << "Step 2. Multiply is by two: " << endl;
+    cout << "Step 3. Add: " << num << endl;
+    cout << "Step 4. Divide it by: " << div << endl;
+    cout << "Step 5. Subtract your original starting number: " << endl;
+
+    cout << "when you are ready for me to tell you my number: press(Y)" << endl;
+    cin >> ans;
 
     bool tracker = true;
-    while(tracker)
+    while (tracker)
     {
-        if(ans == 'Y' || ans == 'y')
+        if (ans == 'Y' || ans == 'y')
         {
-            cout << "The secret number is: " << num/2 <<endl;
+            cout << "The secret number is: " << num / 2 << endl;
             cout << "Thank you for playing." << endl;
             tracker = false;
-        }  
+        }
         else
         {
-            cout << "Thank you for playing." <<endl;
+            cout << "Thank you for playing." << endl;
             tracker = false;
         }
     }
-
 }
