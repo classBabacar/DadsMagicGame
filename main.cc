0b3dfcfdb65f8b609057b3c0f16b26a356fefade
Babacar Ndoye <babacarndoye13@gmail.com>
2019-08-17 00:58:02 -0400

    Main

--- main.cc
+++ main.cc
@@ -2,44 +2,35 @@
 #include <stdlib.h>
 #include <iomanip>
 #include <time.h>
+#include <cctype>
+
 using namespace std;
 
 int main()
 {
-    char ans;
     srand(time(NULL));
-    double div = 1;
-    while (div == 1)
-    {
-        //Dividing by 1 breaks the code
-        cout << "hi" << endl;
-        div = rand() % 2 + 1;
-    }
-    double num = rand() % 20 + 4;
+
+    double randomNum = rand() % 50 + 1;
 
     cout << setw(5) << "****YOU HAVE TO USE A PENCIL AND PAPER FOR THIS EXERCISE OR YOUR MIND****" << endl;
     cout << "Step 1. Think of number: " << endl;
-    cout << "Step 2. Multiply is by two: " << endl;
-    cout << "Step 3. Add: " << num << endl;
-    cout << "Step 4. Divide it by: " << div << endl;
+    cout << "Step 2. Double it: " << endl;
+    cout << "Step 3. Add: " << randomNum << endl;
+    cout << "Step 4. Divide it in half: " << endl;
     cout << "Step 5. Subtract your original starting number: " << endl;
 
-    cout << "when you are ready for me to tell you my number: press(Y)" << endl;
+    cout << "Enter (y) to see the magic number: ";
+    char ans;
     cin >> ans;
 
-    bool tracker = true;
-    while (tracker)
+    if (toupper(ans) == 'Y')
     {
-        if (ans == 'Y' || ans == 'y')
-        {
-            cout << "The secret number is: " << num / 2 << endl;
-            cout << "Thank you for playing." << endl;
-            tracker = false;
-        }
-        else
-        {
-            cout << "Thank you for playing." << endl;
-            tracker = false;
-        }
+        cout << "The secret number is: " << randomNum / 2 << endl;
     }
+    else
+    {
+        cout << "I guess you don't want to see the magic." << endl;
+    }
+
+    cout << "Thank you for playing." << endl;
 }
